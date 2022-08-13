95fd26047465a4d6d69e553934668b5208449a48
nickaceves <nickaceves@gmail.com>
2022-08-13 06:12:18 -0700

    More patch

--- main.py
+++ main.py
@@ -520,8 +520,10 @@ if __name__ == "__main__":
         lightning_config = config.pop("lightning", OmegaConf.create())
         # merge trainer cli with config
         trainer_config = lightning_config.get("trainer", OmegaConf.create())
-        # default to ddp
-        trainer_config["accelerator"] = "ddp"
+
+        # TODO(nick): Propagate from config rather than hard code
+        # default to cuda
+        trainer_config["accelerator"] = "cuda"
         for k in nondefault_trainer_args(opt):
             trainer_config[k] = getattr(opt, k)
         if not "gpus" in trainer_config:
